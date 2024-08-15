f72738ef1ea3448fd10cdc1aa2c5ba92fe981589
leenaabdalrahman <1l;2e3e4n5@>
2024-08-15 19:50:09 +0300

    first

---
+++ info.js
@@ -0,0 +1,88 @@
+export const info = [
+  {
+    "id": 1,
+    "title": "Bosnia and Herzegovina",
+    "price": 899.41,
+    "description": "Travel and enjoy with us a vacation surrounded by nature in Bosnia and Herzegovina 8 days (7 nights) between the capital, Sarajevo, and the picturesque nature of Mostar",
+    " Offer includes": "men's clothing",
+    "image": "Bosnia and Herzegovina.jpeg",
+    "titleImg": "titleBosnia.jpg",
+    "offers": [
+        "Reception and farewell at the airport"
+,
+"Hotel accommodation with breakfast"
+,
+"Private car with driver"
+,
+"Movement between cities"
+,
+"Sightseeing tours"
+,
+"SIM cards and internet"
+,
+"Customer service in Arabic in Bosnia"
+      ],
+      "pictures": [
+        "bosnia1.jpg"
+,
+"bosnia2.jpeg"
+,
+"bosnia3.jpg"
+,
+"bosnia4.jpg"
+],
+"rating": {
+      "rate": 3.9,
+      "count": 120
+    }
+  },
+  {
+    "id": 2,
+    "title": "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+    "price": 109.95,
+    "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+    "category": "men's clothing",
+    "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+    "rating": {
+      "rate": 3.9,
+      "count": 120
+    }
+  },
+
+  {
+    "id": 3,
+    "title": "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+    "price": 109.95,
+    "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+    "category": "men's clothing",
+    "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+    "rating": {
+      "rate": 3.9,
+      "count": 120
+    }
+  },
+  {
+    "id": 4,
+    "title": "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+    "price": 109.95,
+    "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+    "category": "men's clothing",
+    "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+    "rating": {
+      "rate": 3.9,
+      "count": 120
+    }
+  },
+  {
+    "id": 5,
+    "title": "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+    "price": 109.95,
+    "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+    "category": "men's clothing",
+    "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+    "rating": {
+      "rate": 3.9,
+      "count": 120
+    }
+  }
+]
